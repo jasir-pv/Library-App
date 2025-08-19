6f1431e30178be74e13d4f4097f8fe9ac3eb8bec
jasir-pv <jasirahsanpv40@gmail.com>
2025-08-19 11:16:23 +0530

    base url changed in index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
+const API = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
 
 const getToken = () => localStorage.getItem("token");
 
