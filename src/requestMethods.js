dff30523ad74bac486d57d82ae8370278155f619
jasir-pv <jasirahsanpv40@gmail.com>
2025-08-19 11:17:34 +0530

    base url changed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config(); 
-const BASE_URL = process.env.BASE_URL
+
+
+const BASE_URL = process.env.REACT_APP_BASE_URL
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MjIyOWEyMTc0Njg0ZTcyYjE2MzY5MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTczMDM2NTkwNCwiZXhwIjoxNzMwNjI1MTA0fQ.F_eRhBlPEtuZHHEa9Vt5nj5xg0ANosn0UDTN3IX7YKo"
 
 export const publicRequest = axios.create({
